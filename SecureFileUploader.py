eff3c25104c5783e21f7015adf97e0b33ff98dd1
rschneid1 <55038099+rschneid1@users.noreply.github.com>
2021-02-17 17:22:58 -0500

    Add functions

--- SecureFileUploader.py
+++ SecureFileUploader.py
@@ -1,1 +1,23 @@
+# Author: Ryan Schneider
 # Contains the functions for secure file uploader
+# Authentication is provided through specific keys that are auto-generated
+import json
+
+def userGen(account, key):
+  # generate new user and various requirements for new user
+  key = 1
+  return key
+
+def create(tree, account, key):
+  # search known account tree
+  return 1
+
+def update(arrayOfTrees, fields , account, key):
+  return 1
+
+def read(tree, fields, amount, account, key):
+  return 1
+
+
+
+  
