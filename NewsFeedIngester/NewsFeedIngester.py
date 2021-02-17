82042fd3915f7bc6306d212ff74c4e5a9bc9b2a4
rschneid1 <55038099+rschneid1@users.noreply.github.com>
2021-02-17 18:01:11 -0500

    Update NewsFeedIngester.py

--- NewsFeedIngester/NewsFeedIngester.py
+++ NewsFeedIngester/NewsFeedIngester.py
@@ -20,11 +20,15 @@ def ingestTXT(file):
   return jsonObj
 
 def ingestJPEG(file, fields):
+  # read JPEG and detect metadata to fill out JSON object
+  return 1
+
+def ingestMP4(file, fields):
+  # read MP4 and detect metadata to fill out JSON object
   return 1
 # Provides fields for user to fill out their own metadata
 def ingestCustom(fields):
   return 1
-
 # User enters data and desired fields and it creates object, or can use standard fields
 def createFields():
   return 1
