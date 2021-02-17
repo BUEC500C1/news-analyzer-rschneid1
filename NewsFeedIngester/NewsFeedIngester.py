0f720774987cdfbb743b9d010b768dd361113e82
rschneid1 <55038099+rschneid1@users.noreply.github.com>
2021-02-17 17:55:29 -0500

    Update NewsFeedIngester.py

--- NewsFeedIngester/NewsFeedIngester.py
+++ NewsFeedIngester/NewsFeedIngester.py
@@ -1,3 +1,32 @@
 # Author: Ryan Schneider
 # functions for the news feed ingestor
 
+# SUPPORTED FILE TYPES: PDF, .doc, .docx, 
+import json
+
+def ingestPDF(file):
+  # read PDF and detect metadata to fill out JSON object
+  jsonObj = file
+  return jsonObj
+
+def ingestMSDOC(file):
+  # read DOC/DOCX and detect metadata to fill out JSON object
+  jsonObj = file
+  return jsonObj
+
+def ingestTXT(file):
+  # read .txt and detect metadata to fill out JSON object
+  jsonObj = file
+  return jsonObj
+
+def ingestJPEG(file, fields):
+  return 1
+# Provides fields for user to fill out their own metadata
+def ingestCustom(fields):
+  return 1
+
+# User enters data and desired fields and it creates object, or can use standard fields
+def createFields():
+  return 1
+  
+
