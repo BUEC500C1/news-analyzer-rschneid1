7f752e11866897a9460b9fe3d16b795fa592154c
rschneid1 <55038099+rschneid1@users.noreply.github.com>
2021-02-21 17:24:33 -0500

    Update NewsFeedIngester_test.py

--- NewsFeedIngester/NewsFeedIngester_test.py
+++ NewsFeedIngester/NewsFeedIngester_test.py
@@ -8,3 +8,27 @@ def test_ingestPDF():
 def test_ingestMSDOC():
   file = "www.google.com/user/schneid/file.doc"
   assert ingestMSDOC(file) == file
+
+def test_ingestTXT():
+  file = "www.google.com/user/schneid/file.txt"
+  assert ingestTXT(file) == file
+
+def test_ingestJPEG():
+  file = "www.google.com/user/schneid/file.jpeg"
+  fields = "author: Ryan Schneider"
+  assert ingestJPEG(file, fields) == 1
+
+def test_ingestMP4(file, fields):
+  file = "www.google.com/user/schneid/file.jpeg"
+  fields = "author: Ryan Schneider"
+  assert ingestMP4file, fields) == 1
+  
+  
+def test_ingestCustom(fields):
+  fields = "author: Ryan Schneider"
+  assert ingestCustom(fields) == 1
+
+  
+# User enters data and desired fields and it creates object, or can use standard fields
+def test_createFields():
+  assert createFields() == 1
