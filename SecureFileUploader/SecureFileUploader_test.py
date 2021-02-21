5201a1eda88a75cd30a96696880cc28cfd38af80
rschneid1 <55038099+rschneid1@users.noreply.github.com>
2021-02-21 18:46:28 -0500

    Update SecureFileUploader_test.py

--- SecureFileUploader/SecureFileUploader_test.py
+++ SecureFileUploader/SecureFileUploader_test.py
@@ -1,2 +1,28 @@
 from SecureFileUploader import *
 
+def test_userGen():
+  account = "Ryan Schneider"
+  key = 1
+  assert userGen(account,key) == key
+  
+def test_create():
+  account = "Ryan Schneider"
+  key = 1
+  tree = "/users/ryanSchneid/PDFfield"
+  assert create(tree, account, key) == 1
+
+def test_update():
+  arrayOfTrees = ["users", "ryan"]
+  fields = "pdf"
+  account = "Ryan Schneider"
+  key = 1
+  assert update(arrayOfTrees, fields, account, key) == 1
+
+def read():
+  tree = "/users/ryanSchneid/PDFfield"
+  fields = "pdf"
+  amount = 8
+  account = "Ryan Schneider"
+  key = 1
+  assert read(tree, fields, amount, account, key)
+
