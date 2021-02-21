c1db021f67039ae9a5eb389f66b7e947ffe0458a
rschneid1 <55038099+rschneid1@users.noreply.github.com>
2021-02-21 17:19:48 -0500

    Create tests

--- NewsFeedIngester/NewsFeedIngester_test.py
+++ NewsFeedIngester/NewsFeedIngester_test.py
@@ -1,3 +1,10 @@
 from NewsFeedIngester import *
 
 # test various functions
+def test_ingestPDF():
+  file = "www.google.com/user/schneid/pdf1"
+  assert ingestPDF(file) == file
+
+def test_ingestMSDOC():
+  file = "www.google.com/user/schneid/file.doc"
+  assert ingestMSDOC(file) == file
