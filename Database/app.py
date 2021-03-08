61f3b51cf203a25ea61cb32625fcf39a0d7d29cb
rschneid1 <55038099+rschneid1@users.noreply.github.com>
2021-03-08 12:32:04 -0500

    Update app.py
    Stub for database

--- Database/app.py
+++ Database/app.py
@@ -8,7 +8,6 @@ from flask_restful import Resource, Api
 
 # mongo
 from pymongo import MongoClient
-client = MongoClient()
 
 # Set up Mongo
 client = MongoClient('localhost',27017)
@@ -17,13 +16,27 @@ db = client.news_database
 app = Flask(__name__)
 api = Api(app)
 
+todos = {}
+
 # Basic Put and Get functions
 class SecureFileUploader(Resource):
-    def get(self, todo_id):
+    def read(self, todo_id):
+        # pymongo performing a query from database
         return {'todo:id': todos[todo_id]}
-    
-    def put(self, todo_id):
+
+    def update(self, todo_id):
         todos[todo_id] = request.form['data']
+        # pymongo command updating database
+        return {todo_id: todos[todo_id]}
+
+    def userGen(self, todo_id):
+        todos[todo_id] = request.form['data']
+        # pymongo command creating a user account in database
+        return {todo_id: todos[todo_id]}
+
+    def create(self, todo_id):
+        todos[todo_id] = request.form['data']
+        # pymongo adding a document to database
         return {todo_id: todos[todo_id]}
 
 api.add_resource(SecureFileUploader,'/<string:todo_id>')
