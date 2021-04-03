87a107c66ca03c2cb3faa2669c5281b0bb2cec78
rschneid1 <55038099+rschneid1@users.noreply.github.com>
2021-04-03 15:16:29 -0400

    Update SecureFileUploader.py

--- SecureFileUploader/SecureFileUploader.py
+++ SecureFileUploader/SecureFileUploader.py
@@ -7,32 +7,30 @@ from requests import put,get
 
 # Creating a user
 def createUser(first, last, key):
-  put(http://localhost/document/create/user, data={'key': key ,'first': first, 'last' : last }).json()
+  put('http://127.0.0.1:5000/document/create/user', data={'key': key ,'first': first, 'last' : last })
   return key
 
 # Create a document
 def createDoc(key, file):
-  put(http://localhost/document/create/doc), data={'key': key, 'file': file}).json()  
+  put("http://127.0.0.1:5000/document/create/doc", data={'key': key, 'file': file})  
   return key
 
 # update user
 def updateUser(key, form, data):
-  put(http://localhost/document/update/user, data={'key': key ,'form': form, 'last' : data }).json()
+  put("http://127.0.0.1:5000/document/update/user", data={'key': key ,'form': form, 'last' : data })
   return key
 
 # update document
 def updateDoc(key, file, form, data):
-  put(http://localhost/document/update/doc, data={'key': key ,'file': file, 'form' : form, 'data': data }).json()
+  put("http://127.0.0.1:5000/document/update/doc", data={'key': key ,'file': file, 'form' : form, 'data': data })
   return key
 
 # query from user collection
 def readUser(key):
-  put(http://localhost/document/query/user, data={'key': key }).json()
+  put("http://127.0.0.1:5000/document/query/user", data={'key': key })
   return "Returned Result ! \n"
 
 # query from document collection
 def readDoc(key, file):
-  put(http://localhost/document/query/doc, data={'key': key ,'file': first}).json()
+  put("http://127.0.0.1:5000/document/query/doc", data={'key': key ,'file': first})
   return "Returned Result ! \n"
-
-  
