fbe22373027cb1104e571f472f380b746f27d03a
rschneid1 <55038099+rschneid1@users.noreply.github.com>
2021-04-12 01:11:37 -0400

    Update SecureFileUploader.py

--- SecureFileUploader/SecureFileUploader.py
+++ SecureFileUploader/SecureFileUploader.py
@@ -7,30 +7,36 @@ from requests import put,get
 
 # Creating a user
 def createUser(first, last, key):
-  put('http://127.0.0.1:5000/document/create/user', data={'key': key ,'first': first, 'last' : last })
-  return key
+  r = put('http://127.0.0.1:5000/document/create/user', data={'key': key ,'first': first, 'last' : last })
+  output = r.content
+  return output.decode("utf-8")
 
 # Create a document
 def createDoc(key, file):
-  put("http://127.0.0.1:5000/document/create/doc", data={'key': key, 'file': file})  
-  return key
+  r = put("http://127.0.0.1:5000/document/create/doc", data={'key': key, 'file': file})  
+  output = r.content
+  return output.decode("utf-8")
 
 # update user
 def updateUser(key, form, data):
-  put("http://127.0.0.1:5000/document/update/user", data={'key': key ,'form': form, 'last' : data })
-  return key
+  r = put("http://127.0.0.1:5000/document/update/user", data={'key': key ,'form': form, 'data' : data })
+  output = r.content
+  return output.decode("utf-8")
 
 # update document
 def updateDoc(key, file, form, data):
-  put("http://127.0.0.1:5000/document/update/doc", data={'key': key ,'file': file, 'form' : form, 'data': data })
-  return key
+  r = put("http://127.0.0.1:5000/document/update/doc", data={'key': key ,'file': file, 'form' : form, 'data': data })
+  output = r.content
+  return output.decode("utf-8")
 
 # query from user collection
 def readUser(key):
-  put("http://127.0.0.1:5000/document/query/user", data={'key': key })
-  return "Returned Result ! \n"
+  r = put("http://127.0.0.1:5000/document/query/user", data={'key': key })
+  output = r.content
+  return output.decode("utf-8")
 
 # query from document collection
 def readDoc(key, file):
-  put("http://127.0.0.1:5000/document/query/doc", data={'key': key ,'file': first})
-  return "Returned Result ! \n"
+  r = put("http://127.0.0.1:5000/document/query/doc", data={'key': key ,'file': file})
+  output = r.content
+  return output.decode("utf-8")
